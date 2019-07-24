722689b9f8c56b1c2d60e104a4bba36a9ff5a67d
miemie <wonderming@vip.qq.com>
2019-07-24 15:23:40 +0800

    init1

--- mipac4j-core/src/main/java/com/baomidou/mipac4j/core/client/TokenIndirectClient.java
+++ mipac4j-core/src/main/java/com/baomidou/mipac4j/core/client/TokenIndirectClient.java
@@ -1,24 +1,34 @@
 package com.baomidou.mipac4j.core.client;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.pac4j.core.client.IndirectClient;
 import org.pac4j.core.credentials.TokenCredentials;
 import org.pac4j.core.credentials.authenticator.Authenticator;
 import org.pac4j.core.credentials.extractor.CredentialsExtractor;
 import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.core.redirect.RedirectAction;
 
 /**
  * @author miemie
  * @since 2019-07-24
  */
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class TokenIndirectClient extends IndirectClient<TokenCredentials, CommonProfile> {
 
-    public TokenIndirectClient(final CredentialsExtractor<TokenCredentials> credentialsExtractor, final Authenticator<TokenCredentials> tokenAuthenticator) {
+    public TokenIndirectClient(final String loginUrl, final CredentialsExtractor<TokenCredentials> credentialsExtractor,
+                               final Authenticator<TokenCredentials> tokenAuthenticator) {
         defaultCredentialsExtractor(credentialsExtractor);
         defaultAuthenticator(tokenAuthenticator);
+        defaultRedirectActionBuilder(ctx -> {
+            final String finalLoginUrl = getUrlResolver().compute(loginUrl, ctx);
+            return RedirectAction.redirect(finalLoginUrl);
+        });
     }
 
     @Override
     protected void clientInit() {
-
+        // ignore
     }
 }
