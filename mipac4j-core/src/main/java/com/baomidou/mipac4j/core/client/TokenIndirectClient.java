32bc52354c12a25fd6519c840e54fb3c9f9ebfa4
miemie <wonderming@vip.qq.com>
2019-07-24 12:56:37 +0800

    init1

--- mipac4j-core/src/main/java/com/baomidou/mipac4j/core/client/TokenIndirectClient.java
+++ mipac4j-core/src/main/java/com/baomidou/mipac4j/core/client/TokenIndirectClient.java
@@ -2,6 +2,8 @@ package com.baomidou.mipac4j.core.client;
 
 import org.pac4j.core.client.IndirectClient;
 import org.pac4j.core.credentials.TokenCredentials;
+import org.pac4j.core.credentials.authenticator.Authenticator;
+import org.pac4j.core.credentials.extractor.CredentialsExtractor;
 import org.pac4j.core.profile.CommonProfile;
 
 /**
@@ -9,6 +11,11 @@ import org.pac4j.core.profile.CommonProfile;
  * @since 2019-07-24
  */
 public class TokenIndirectClient extends IndirectClient<TokenCredentials, CommonProfile> {
+
+    public TokenIndirectClient(final CredentialsExtractor<TokenCredentials> credentialsExtractor, final Authenticator<TokenCredentials> tokenAuthenticator) {
+        defaultCredentialsExtractor(credentialsExtractor);
+        defaultAuthenticator(tokenAuthenticator);
+    }
 
     @Override
     protected void clientInit() {
