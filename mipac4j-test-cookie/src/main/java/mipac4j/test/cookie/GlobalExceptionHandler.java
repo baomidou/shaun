684b72fead532ea732dd924e30aabd5ed070906c
miemieYaho <wonderming@vip.qq.com>
2019-07-25 21:19:13 +0800

    改造

---
+++ mipac4j-test-cookie/src/main/java/mipac4j/test/cookie/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package mipac4j.test.cookie;
+
+import org.pac4j.core.exception.HttpAction;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 全局异常处理器
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({HttpAction.class})
+    public String bindException(HttpAction action) {
+        if (action.getCode() == 403) {
+            return "你没有权限";
+        }
+        return "你没登录";
+    }
+}
