97b753a02e987e8075cfcd02caa187446e33329e
miemie <wonderming@vip.qq.com>
2019-07-30 16:29:08 +0800

    暂不支持前后不分离的

--- mipac4j-core/src/main/java/com/baomidou/mipac4j/core/interceptor/MIPac4jInterceptor.java
+++ mipac4j-core/src/main/java/com/baomidou/mipac4j/core/interceptor/MIPac4jInterceptor.java
@@ -1,10 +1,49 @@
 package com.baomidou.mipac4j.core.interceptor;
 
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import com.baomidou.mipac4j.core.context.J2EContextFactory;
+import com.baomidou.mipac4j.core.filter.Pac4jFilter;
+import lombok.Data;
+import org.pac4j.core.context.J2EContext;
+import org.pac4j.core.context.session.SessionStore;
+import org.pac4j.core.util.CommonHelper;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author miemie
  * @since 2019-07-29
  */
-public class MIPac4jInterceptor extends HandlerInterceptorAdapter {
+@Data
+public class MIPac4jInterceptor implements HandlerInterceptor, InitializingBean {
+
+    private List<Pac4jFilter> filterList = Collections.emptyList();
+
+    private SessionStore<J2EContext> sessionStore;￿
+
+    private J2EContextFactory j2EContextFactory;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (!CorsUtils.isPreFlightRequest(request)) {
+            final J2EContext context = j2EContextFactory.applyContext(request, response, sessionStore);
+            for (Pac4jFilter filter : filterList) {
+                if (!filter.goOnChain(context)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        CommonHelper.assertNotNull("sessionStore", sessionStore);
+        CommonHelper.assertNotNull("j2EContextFactory", j2EContextFactory);
+    }
 }
