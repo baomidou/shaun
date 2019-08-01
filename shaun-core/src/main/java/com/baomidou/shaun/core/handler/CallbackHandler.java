672eb96f75fceeb4036efb0903406d8bc493e496
miemie <wonderming@vip.qq.com>
2019-08-01 14:44:35 +0800

    升级pac4j到4版本,慢慢改造

--- shaun-core/src/main/java/com/baomidou/shaun/core/handler/CallbackHandler.java
+++ shaun-core/src/main/java/com/baomidou/shaun/core/handler/CallbackHandler.java
@@ -1,13 +1,13 @@
 package com.baomidou.shaun.core.handler;
 
-import org.pac4j.core.context.J2EContext;
-import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.core.context.JEEContext;
+import org.pac4j.core.profile.UserProfile;
 
 /**
  * @author miemie
  * @since 2019-07-26
  */
-public interface CallbackHandler<R extends CommonProfile> {
+public interface CallbackHandler<R extends UserProfile> {
 
     /**
      * callback 之后对返回获取到的 profile 转换成自己的 profile
@@ -16,5 +16,5 @@ public interface CallbackHandler<R extends CommonProfile> {
      * @param profile callback 获取到的 profile
      * @return 自己的 profile
      */
-    R callBack(J2EContext context, CommonProfile profile);
+    R callBack(JEEContext context, UserProfile profile);
 }
