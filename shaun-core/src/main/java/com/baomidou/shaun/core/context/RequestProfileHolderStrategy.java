d858ff54088ff938fe37df69ac0f9a55e7be37a8
miemie <wonderming@vip.qq.com>
2020-11-30 18:01:39 +0800

    license

--- shaun-core/src/main/java/com/baomidou/shaun/core/context/RequestProfileHolderStrategy.java
+++ shaun-core/src/main/java/com/baomidou/shaun/core/context/RequestProfileHolderStrategy.java
@@ -1,13 +1,27 @@
+/**
+ * Copyright 2019-2020 baomidou (wonderming@vip.qq.com)
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.baomidou.shaun.core.context;
 
-import javax.servlet.http.HttpServletRequest;
-
+import com.baomidou.shaun.core.profile.TokenProfile;
 import org.pac4j.core.util.Pac4jConstants;
 import org.springframework.util.Assert;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import com.baomidou.shaun.core.profile.TokenProfile;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author miemie
